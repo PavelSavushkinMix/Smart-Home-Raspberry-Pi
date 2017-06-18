fc7088f9b21866dfd6fb5195c43b0b3479f989e1
oleg <oleg.kaliuzhny@gmail.com>
2017-06-19 02:40:40 +0300

    Added: scripts for notifications and switchers.\nChanged: colors for notifications to white; no bckgrd-clr for switcher:hover

--- View/js/script.js
+++ View/js/script.js
@@ -1,5 +1,8 @@
 function afterLoad() {
     'use strict';
+
+    //Уведомления
+    //Смена цвета на зеленоватый, если уведомлений больше 0
     var notificationsAmount = document.getElementById('notifications-amount').innerHTML;
     notificationsAmount = notificationsAmount.replace('(', '');
     notificationsAmount = notificationsAmount.replace(')', '');
@@ -7,6 +10,46 @@ function afterLoad() {
     {
         document.getElementById('btn-notification').style.color = "greenyellow";
     }
+    //
 
+
+    //Смена цвета кнопок управления после загрузки страницы
+    var switchers = document.getElementsByClassName("switch-button");  
+    for (var i = 0; i < switchers.length; i++)
+    {
+        if (switchers[i].innerHTML === "Включено")
+        {
+            switchers[i].style.backgroundColor = "lightgreen";
+        }
+    }
+    //
 }
 
+
+function switchPressed(num) {
+    /*
+    TO-DO
+    Отправка запроса на сервер
+    
+    */
+    var switcher = document.getElementById("room-switcher" + num.toString());
+    if (switcher.innerHTML === "Включено")
+    {
+        switcher.innerHTML = "Выключено";
+        switcher.style.backgroundColor = "burlywood";
+    }
+    else
+    {
+        switcher.innerHTML = "Включено";
+        switcher.style.backgroundColor = "lightgreen";
+    }
+}
+
+function notificationsChecked() {
+    /*
+    TO-DO
+    Отправка запроса на сервер
+    
+    */
+    document.getElementById('btn-notification').style.color = "#bbb";
+}
